dd28cf9e8a20f2e0e1c2a550fa2183a55bafd96f
Jamie Gaskins <jgaskins@hey.com>
2025-03-23 18:15:15 -0500

    Simplify pubsub example

--- examples/pub_sub.cr
+++ examples/pub_sub.cr
@@ -1,26 +1,11 @@
 require "../src/nats"
 
 nats = NATS::Client.new
+# Ensure we close the client explicitly to flush any buffered messages
+at_exit { nats.close }
 
 nats.subscribe "foo" do |msg|
   pp msg
 end
 
 nats.publish "foo", "bar"
-
-sleep 1.second
-exit 0
-
-count = 0
-start = Time.monotonic
-loop do
-  nats.publish "foo", "bar"
-  count += 1
-
-  if count >= 50_000
-    pp rate: (count // (Time.monotonic - start).total_seconds).format
-    Fiber.yield
-    count = 0
-    start = Time.monotonic
-  end
-end
