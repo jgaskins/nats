24373d90025810fa0e571b3478ebd99f4cb6d06b
Jamie Gaskins <jgaskins@hey.com>
2025-07-07 00:15:51 -0500

    Add the Services API
    https://github.com/nats-io/nats-architecture-and-design/blob/main/adr/ADR-32.md

---
+++ spec/services_spec.cr
@@ -0,0 +1,207 @@
+require "./spec_helper"
+require "../src/services"
+
+nats = NATS::Client.new
+  .on_error { |ex| pp ex }
+
+describe NATS::Services do
+  it "creates a service" do
+    name = "test-service-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    svc.name.should eq name
+  end
+
+  it "creates endpoints" do
+    name = "test-endpoint-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+    root = svc.add_group(name)
+    root.add_endpoint "upcase" do |request|
+      nats.reply request, request.data_string.upcase
+    end
+    root.add_endpoint "downcase" do |request|
+      nats.reply request, request.data_string.downcase
+    end
+
+    unless response = nats.request "#{name}.upcase", "hello"
+      raise "Did not receive a response from the endpoint"
+    end
+    response.data_string.should eq "HELLO"
+    unless response = nats.request "#{name}.downcase", "HELLO"
+      raise "Did not receive a response from the endpoint"
+    end
+    response.data_string.should eq "hello"
+  end
+
+  it "cannot create an endpoint with an invalid name" do
+    name = "test-endpoint-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+    root = svc.add_group(name)
+    expect_raises ArgumentError do
+      root.add_endpoint "get.*" do |request|
+        nats.reply request, request.data_string.upcase
+      end
+    end
+  end
+
+  it "responds to pings across all services" do
+    name = "test-ping-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    nats.request("$SRV.PING", "").should_not be_nil
+  end
+
+  it "responds to pings at the service level" do
+    name = "test-ping-service-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    nats.request("$SRV.PING.#{name}", "").should_not be_nil
+  end
+
+  it "responds to pings at the service id level" do
+    name = "test-ping-service-id-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    nats.request("$SRV.PING.#{name}.#{svc.id}", "").should_not be_nil
+  end
+
+  it "responds with stats across all services" do
+    name = "test-stats-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    nats.request("$SRV.STATS", "").should_not be_nil
+  end
+
+  it "responds with stats for the service" do
+    name = "test-stats-service-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+    group = svc.add_group name
+    group.add_endpoint "foo" do |request|
+      nats.reply request, "stuff"
+    end
+    nats.request "#{name}.foo", ""
+
+    unless response = nats.request("$SRV.STATS.#{name}", "")
+      raise "Did not receive a response from the endpoint"
+    end
+    stats = JSON.parse(response.data_string)
+
+    stats["endpoints"][0]["num_requests"].should eq 1
+  end
+
+  it "responds with stats for the service with the given id" do
+    name = "test-stats-service-id-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+    group = svc.add_group name
+    group.add_endpoint "foo" do |request|
+      nats.reply request, "stuff"
+    end
+    nats.request "#{name}.foo", ""
+
+    unless response = nats.request("$SRV.STATS.#{name}.#{svc.id}", "")
+      raise "Did not receive a response from the endpoint"
+    end
+    stats = JSON.parse(response.data_string)
+
+    stats["endpoints"][0]["num_requests"].should eq 1
+  end
+
+  it "responds with info for all services" do
+    name = "test-info-service-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    nats.request("$SRV.INFO", "").should_not be_nil
+  end
+
+  it "responds with info for this specific service" do
+    name = "test-info-service-id-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    unless response = nats.request("$SRV.INFO.#{name}", "")
+      raise "Did not receive a response from the endpoint"
+    end
+    info = JSON.parse(response.data_string)
+
+    info["type"].should eq "io.nats.micro.v1.info_response"
+    info["name"].should eq name
+    info["id"].should eq svc.id
+    info["description"].should eq "does a thing"
+    info["endpoints"].raw.should be_a Array(JSON::Any)
+  end
+
+  it "responds with info for this service with the specific id" do
+    name = "test-info-service-id-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    unless response = nats.request("$SRV.INFO.#{name}.#{svc.id}", "")
+      raise "Did not receive a response from the endpoint"
+    end
+    info = JSON.parse(response.data_string)
+
+    info["type"].should eq "io.nats.micro.v1.info_response"
+  end
+
+  it "responds with Nats-Service-Error and Nats-Service-Error-Code if an exception is raised in the block" do
+    name = "test-service-errors-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+    group = svc.add_group name
+    endpoint = group.add_endpoint "oops" do
+      raise "hell"
+    end
+
+    unless response = nats.request("#{name}.oops", "")
+      raise "Did not receive a response from the endpoint"
+    end
+
+    response.headers["Nats-Service-Error-Code"].should eq "500"
+    response.headers["Nats-Service-Error"].should eq "hell"
+    endpoint.total_errors.should eq 1
+    endpoint.last_error.should eq "hell"
+  end
+
+  it "allows nested groups" do
+    name = "test-nested-groups-#{UUID.v7}"
+    svc = nats.services.add name,
+      version: "0.1.0",
+      description: "does a thing"
+
+    # This also tests that we can pass a block to add_group, so that's neat
+    svc.add_group name do |group|
+      group.add_group "a" do |a|
+        a.add_endpoint "stuff" do |msg|
+          nats.reply msg, "yep"
+        end
+      end
+    end
+
+    nats.request("#{name}.a.stuff", "")
+      .try(&.data_string)
+      .should eq "yep"
+  end
+end
---
+++ src/services.cr
@@ -0,0 +1,359 @@
+require "./nats"
+require "./nuid"
+
+module NATS
+  # The [NATS Services API](https://docs.nats.io/using-nats/developer/services)
+  # allows you to build microservices using NATS instead of HTTP. Since NATS has
+  # the concept of request/reply messages through a decentralized message broker,
+  # you don't need to deal with domain names, load-balancing, service meshes, or
+  # other concepts that you might have to implement in HTTP-based microservices. NATS takes care of all of those for you.
+  #
+  # ```
+  # inventory = nats.services.add "inventory",
+  #   version: "0.1.0",
+  #   description: "Manage store inventory"
+  #
+  # # Add the top-level subject namespace for the service
+  # inventory.add_group "inventory" do |inv|
+  #   # Add a namespace specifically for managing products
+  #   inv.add_group "products" do |products|
+  #     # Create a product. Receives a CreateProductRequest and returns a Product.
+  #     # Example using the NATS CLI:
+  #     #   nats req inventory.products.create '{"name":"Computer","description":"It is a computer","price_cents":200000}'
+  #     products.add_endpoint "create" do |request|
+  #       # Deserialize the request body and make a product out of it
+  #       product = CreateProductRequest.from_json(request.data_string).product
+  #       # Save the product to our KV store
+  #       products_kv[product.id.to_s] = product.to_json
+  #       # Reply with the serialized product
+  #       nats.reply request, product.to_json
+  #     end
+  #
+  #     # Return the product with the given id passed in the subject.
+  #     # Example: inventory.products.get.0197e31f-3ffa-7fd7-8512-a331a20e5ae3
+  #     # Equivalent to the following HTTP request:
+  #     #   GET /inventory/products/0197e31f-3ffa-7fd7-8512-a331a20e5ae3
+  #     products.add_endpoint "show", subject: "get.*" do |request|
+  #       # Split the subject to get the id
+  #       _, _, _, product_id = request.subject.split('.')
+  #
+  #       # Fetch the product from the `inventory-products` KV bucket
+  #       nats.reply request, products_kv[product_id]
+  #     end
+  #   end
+  # end
+  # ```
+  class Services
+    @nats : Client
+
+    NAME_PATTERN = /\A[\w\-]+\z/
+
+    def initialize(@nats)
+    end
+
+    # Register a service with the NATS server.
+    #
+    # ```
+    # inventory = nats.services.add "inventory",
+    #   version: "0.1.0",
+    #   description: "Manage inventory"
+    # ```
+    def add(name : String, *, version : String, description : String, metadata : Hash(String, String) = {} of String => String)
+      unless name =~ NAME_PATTERN
+        raise ArgumentError.new("NATS service names may only contain A-Z, a-z, 0-9, dash, and underscore characters.")
+      end
+      started = Time.utc.to_rfc3339
+      id = NUID.next
+      service = Service.new(
+        nats: @nats,
+        id: id,
+        name: name,
+        version: version,
+        description: description,
+        metadata: metadata,
+      )
+
+      [
+        "$SRV.PING",
+        "$SRV.PING.#{name}",
+        "$SRV.PING.#{name}.#{id}",
+      ].each do |subject|
+        service.subscriptions << @nats.subscribe subject do |msg|
+          @nats.reply msg, {
+            type:     "io.nats.micro.v1.ping_response",
+            name:     name,
+            id:       id,
+            version:  version,
+            metadata: metadata,
+          }.to_json
+        end
+      end
+
+      [
+        "$SRV.STATS",
+        "$SRV.STATS.#{name}",
+        "$SRV.STATS.#{name}.#{id}",
+      ].each do |subject|
+        service.subscriptions << @nats.subscribe subject do |msg|
+          @nats.reply msg, {
+            type:      "io.nats.micro.v1.stats_response",
+            name:      name,
+            id:        id,
+            version:   version,
+            metadata:  metadata,
+            endpoints: service.endpoint_stats,
+            started:   started,
+          }.to_json
+        end
+      end
+
+      [
+        "$SRV.INFO",
+        "$SRV.INFO.#{name}",
+        "$SRV.INFO.#{name}.#{id}",
+      ].each do |subject|
+        service.subscriptions << @nats.subscribe subject do |msg|
+          @nats.reply msg, {
+            type:        "io.nats.micro.v1.info_response",
+            name:        name,
+            id:          id,
+            version:     version,
+            description: description,
+            metadata:    metadata,
+            endpoints:   service.endpoint_info,
+          }.to_json
+        end
+      end
+
+      service
+    end
+  end
+
+  # The `Service` represents a NATS service that was created by the `Services`
+  # API. It exists to hang your endpoints on:
+  #
+  # ```
+  # inventory = nats.services.add "inventory",
+  #   version: "0.1.0",
+  #   description: "Manage inventory"
+  # inventory.add_endpoint "inventory.products.get", subject: "inventory.products.get.*" do |request|
+  #   _, _, _, id
+  #   nats.reply request, ProductQuery.new.find(id).to_json
+  # end
+  # ```
+  class Service
+    @nats : Client
+    getter id : String
+    getter name : String
+    getter version : String
+    getter description : String
+    getter metadata : Hash(String, String)
+    getter subscriptions : Array(Subscription)
+    getter endpoints : Array(Endpoint) = [] of Endpoint
+    @groups = [] of Group
+    protected getter error_handler : ErrorHandler
+
+    alias ErrorHandler = Exception, Message ->
+
+    # :nodoc:
+    def initialize(@nats, @id, @name, @version, @description, @metadata, @subscriptions = [] of Subscription)
+      @error_handler = ErrorHandler.new do |error, message|
+        @nats.reply message, "",
+          headers: Headers{
+            "Nats-Service-Error-Code" => "500",
+            "Nats-Service-Error"      => error.message.try(&.gsub(/\s+/, ' ')) || "Internal error",
+          }
+      end
+    end
+
+    # ```
+    # service.add_endpoint "endpoint-name", subject: "endpoint.subject" do |request|
+    #   nats.reply request, "reply goes here"
+    # end
+    # ```
+    def add_endpoint(
+      name : String,
+      *,
+      subject : String = name,
+      queue_group : String = "q",
+      &block : Message ->
+    ) : Endpoint
+      unless name =~ Services::NAME_PATTERN
+        raise ArgumentError.new("Endpoint names can only contain letter, number, dash, and underscore characters")
+      end
+
+      endpoint = uninitialized Endpoint
+      subscription = @nats.subscribe subject, queue_group: queue_group do |msg|
+        endpoint.request msg do
+          block.call msg
+        end
+      end
+
+      endpoint = Endpoint.new(
+        nats: @nats,
+        service: self,
+        name: name,
+        subject: subject,
+        queue_group: queue_group,
+        subscription: subscription,
+      )
+      endpoints << endpoint
+
+      endpoint
+    end
+
+    # Add a `Group` and yield it to the block.
+    #
+    # ```
+    # service.add_group "products" do |inventory|
+    #   inventory.add_endpoint "create" do |request|
+    #     nats.reply request, "reply goes here"
+    #   end
+    # end
+    # ```
+    def add_group(name : String, &)
+      yield add_group(name)
+    end
+
+    # Add a `Group` of endpoints.
+    def add_group(name : String)
+      group = Group.new(@nats, self, name)
+      @groups << group
+      group
+    end
+
+    # Execute the given block for all exceptions raised in this service's
+    # endpoints. This is useful for returning a common error response.
+    #
+    # ```
+    # service.on_error do |exception, message|
+    #   nats.reply message, "", headers: NATS::Headers{
+    #     "Nats-Service-Error-Code" => "500",
+    #     "Nats-Service-Error"      => "Unexpected error: #{exception.message}",
+    #   }
+    # end
+    # ```
+    def on_error(&@error_handler : ErrorHandler)
+      self
+    end
+
+    # :nodoc:
+    def endpoint_stats
+      endpoints.map do |endpoint|
+        {
+          name:                    endpoint.name,
+          subject:                 endpoint.subject,
+          queue_group:             endpoint.queue_group,
+          num_requests:            endpoint.total_requests,
+          num_errors:              endpoint.total_errors,
+          last_error:              endpoint.last_error,
+          data:                    nil,
+          processing_time:         endpoint.total_processing_time.total_nanoseconds.to_i64,
+          average_processing_time: endpoint.average_processing_time.total_nanoseconds.to_i64,
+        }
+      end
+    end
+
+    # :nodoc:
+    def endpoint_info
+      endpoints.map do |endpoint|
+        {
+          type:        "io.nats.micro.v1.info_response",
+          name:        endpoint.name,
+          subject:     endpoint.subject,
+          queue_group: endpoint.queue_group,
+        }
+      end
+    end
+
+    # The  `Group` represents a cohesive group of endpoints. Groups are useful
+    # for applying a common prefix to subjects or setting a common queue group.
+    class Group
+      @nats : Client
+      getter service : Service
+      getter name : String
+
+      def initialize(@nats, @service, @name)
+      end
+
+      def add_endpoint(
+        name : String,
+        *,
+        subject : String = name,
+        queue_group : String = "q",
+        &block : Message ->
+      ) : Endpoint
+        service.add_endpoint name,
+          subject: "#{@name}.#{subject}",
+          queue_group: queue_group,
+          &block
+      end
+
+      def add_group(name : String, &)
+        yield add_group(name)
+      end
+
+      def add_group(name : String)
+        Group.new(@nats, @service, "#{@name}.#{name}")
+      end
+    end
+
+    # The `Endpoint` is the request handler. You generally won't interact with
+    # this class directly.
+    class Endpoint
+      @nats : Client
+      getter service : Service
+      getter name : String
+      getter subject : String
+      getter queue_group : String
+      getter subscription : Subscription
+      @total_requests : Atomic(Int64) = Atomic.new(0i64)
+      @total_errors : Atomic(Int64) = Atomic.new(0i64)
+      @total_processing_nanoseconds : Atomic(Int64) = Atomic.new(0i64)
+      getter last_error : String?
+      protected getter error_handler : ErrorHandler
+
+      def initialize(@nats, @service, @name, @subject, @queue_group, @subscription)
+        @error_handler = @service.error_handler
+      end
+
+      def total_requests
+        @total_requests.get
+      end
+
+      def total_errors
+        @total_errors.get
+      end
+
+      def total_processing_time
+        @total_processing_nanoseconds.get.nanoseconds
+      end
+
+      def average_processing_time
+        requests = total_requests
+        return 0.seconds if requests.zero?
+
+        total_processing_time / total_requests
+      end
+
+      protected def request(message : Message, &)
+        start = Time.monotonic
+        begin
+          yield
+        rescue ex
+          @total_errors.add 1
+          @last_error = ex.message || ex.class.name
+          @error_handler.call ex, message
+          raise ex
+        ensure
+          @total_requests.add 1
+          @total_processing_nanoseconds.add (Time.monotonic - start).total_nanoseconds.to_i64
+        end
+      end
+    end
+  end
+
+  class Client
+    getter services : Services { Services.new self }
+  end
+end
