216a35aa363d4b7a4aaa52c859fa94bf95bf1035
Jamie Gaskins <jgaskins@hey.com>
2025-07-21 09:56:54 -0500

    Format nats_spec.cr

--- spec/nats_spec.cr
+++ spec/nats_spec.cr
@@ -377,7 +377,7 @@ describe NATS do
   end
 end
 
-private def run_nats_on(port : Int32, config : String)
+private def run_nats_on(port : Int32, config : String, &)
   Process.run "nats-server", args: "-js --port #{port} --config #{__DIR__}/support/#{config}.conf".split do |process|
     wait_for_nats port
     client = NATS::Client.new(
